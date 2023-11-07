aa1ecfa5b8b43764986c0faf8f4bce03627c3773
kpenaalvarez <136401504+kpenaalvarez@users.noreply.github.com>
2023-11-06 19:36:19 -0800

    Update main.py

--- main.py
+++ main.py
@@ -2,11 +2,14 @@ def main():
 
     original_str = 'Python Programming'
     ##################################################
-    # Comlete your code here
+    sub1 = original_str[0:6]
+    sub2 = original_str[7:18]
+    space = " "
+    merged_str =  sub2 + space + sub1
     ##################################################
 
-    print(sub2)
     print(sub1)
+    print(sub2)
     print(merged_str)
 
     #########################################
